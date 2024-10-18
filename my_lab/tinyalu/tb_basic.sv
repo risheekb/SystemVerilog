e8a3c0aa04e7b18f9673955a589863981bab6412
risheekb <risheek@pdx.edu>
2024-10-17 19:35:34 -0700

    changes to tinyalu

--- my_lab/tinyalu/tb_basic.sv
+++ my_lab/tinyalu/tb_basic.sv
@@ -8,7 +8,7 @@ module tb_basic
     output opcode_t opcode,
     input result_t result,
     input logic done); 
-  
+  result_t result_expected;
   opcode_t expect_q[$];
   initial begin
     @(posedge clk)
@@ -16,7 +16,7 @@ module tb_basic
     repeat(5) @(posedge clk);
     reset_n <= 1'b1;
 
-    repeat(100) begin
+    repeat(10) begin
       fork
         send_transaction();
         check_transaction();
@@ -30,6 +30,7 @@ module tb_basic
       A = $random;
       B = $random;
       opcode = opcode_t'($urandom_range(0,7));
+      expect_q.push_back(opcode);
     endtask
 
     task send_transaction;
@@ -38,49 +39,71 @@ module tb_basic
       generate_transaction();
       wait(done)
       start = 1'b0;
-      expect_q.push_back(opcode);
     endtask
 
     task check_transaction;
-      wait(done)
+      while(expect_q.size() != 0) begin
+      wait(done)#1ns;
       case(expect_q.pop_front())
         NOP: begin
-          if(result != 'z)
+          if(result !== 'z)
             $error("mismatch for NOP");
         end
         ADD: begin
-          if(result != A + B)
+          result_expected = A + B;
+          if(result != result_expected) begin
             $error("mismatch for ADD");
+            $strobe("@%0t mismatch for ADD expected = %0d actual = %0d",$realtime,result_expected,result);
+          end
         end
         SUB: begin
-          if(result !== A - B) begin
+          result_expected = A - B;
+          if(result != result_expected) begin
             $error();
-            $strobe("@%0t mismatch for SUB expected = %0d actual = %0d",$realtime,(A-B),result);
+            $strobe("@%0t mismatch for SUB expected = %0d actual = %0d",$realtime,result_expected,result);
           end
-          else
-            $display("SELF CHECK WORKS");
         end
         NOT: begin
-          if(result != ~A)
+          result_expected = ~A;
+          if(result != result_expected)begin
             $error("mismatch for NOT");
+            $strobe("@%0t mismatch for NOT expected = %0d actual = %0d",$realtime,result_expected,result);
+          end
+        end
+        XOR: begin
+          result_expected = A ^ B;
+          if(result != result_expected) begin
+            $error("mismatch for XOR");
+            $strobe("@%0t mismatch for XOR expected = %d actual = %d",$realtime,result_expected,result);
+          end
         end
         AND: begin
-          if(result != A & B)
+          result_expected = A & B;
+          if(result != result_expected)begin
             $error("mismatch for AND");
+            $strobe("@%0t mismatch for AND expected = %d actual = %d",$realtime,result_expected,result);
+          end
         end
         MUL: begin
-          if(result != A * B)
+          result_expected = A * B;
+          if(result != result_expected) begin
             $error("mismatch for MUL");
+            $strobe("@%0t mismatch for MUL expected = %d actual = %d",$realtime,result_expected,result);
+          end
         end
         INC: begin
-          if(result != (A+B+1'b1))
+          result_expected =A+B+1'b1;
+          if(result != result_expected) begin 
             $error("mismatch for INC");
+            $strobe("@%0t mismatch for INC expected = %d actual = %d",$realtime,result_expected,result);
+          end
         end
+        
       endcase
+      end
     endtask
 
   initial begin
     $monitor("%0t A = %0d B = %0d reset_n = %0d start = %0d opcode = %s done = %0d result = %0d",$realtime,A,B,reset_n,start,opcode,done,result);
-
   end
 endmodule
--- my_lab/tinyalu/tinyalu.sv
+++ my_lab/tinyalu/tinyalu.sv
@@ -10,41 +10,28 @@ module tinyalu
     output logic done); 
 
 
-  result_t temp2,temp1;
-  result_t temp3,temp4;
-  bit done_temp1,done_temp2,done_temp3,done_temp4;
   always_ff @(posedge clk) begin
     if(!reset_n) begin
       result = '0;
       done = 1'b0;
-      temp1 = 'z;
-      temp2 = 'z;
-      temp3 = 'z;
-      temp4 = 'z;
     end
     else begin
       if(start) begin
         case(opcode)
           NOP: begin
-            done <= 1'b1;
+              done <= 1'b1;
           end
           ADD:begin
-            result  <=  A + B;
-            done    =  1'b1;
+              result  <=  A + B;
+              done    =  1'b1;
           end
           SUB:begin
-            if($test$plusargs("ERROR_INJECT")) begin
-              result <= '1;
-              done = 1'b1;
-            end
-            else begin
               result <= A - B;
               done  = 1'b1;
-            end
           end
           NOT:begin
-            result <= ~A;
-            done = 1'b1;
+              result <= ~A;
+              done = 1'b1;
           end
           XOR:begin
             result <= A ^ B;
@@ -69,5 +56,9 @@ module tinyalu
         result = 'z;
       end
     end
+  end
+  initial begin
+    if($test$plusargs("ERROR_INJECT"))
+      force result = $random;
   end
 endmodule
